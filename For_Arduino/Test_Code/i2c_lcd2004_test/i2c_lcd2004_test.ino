52351d6a5040cd863def4907fdaefc2170436ce2
eeenjoy <eeenjoy0514@126.com>
2018-03-28 19:19:48 +0800

    Update i2c_lcd2004_test.ino

--- For_Arduino/Test_Code/i2c_lcd2004_test/i2c_lcd2004_test.ino
+++ For_Arduino/Test_Code/i2c_lcd2004_test/i2c_lcd2004_test.ino
@@ -10,7 +10,7 @@
 // include the library code
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup() 
 {
